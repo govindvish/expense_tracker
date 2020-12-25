aae8ce0b9bb37f38801513d1db352409d59bfa4a
Govind Vishwakarma <vishwakarmagovind22@gmail.com>
2020-12-25 20:03:44 +0530

    Added basic layout of application.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { AddTransaction } from './components/AddTransaction';
+import { Balance } from './components/Balance';
+import { Header } from './components/Header';
+import { IncomeExpense } from './components/IncomeExpense';
+import { TransactionList } from './components/TransactionList';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!!</h1>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </>
   );
 }
 
