117c75d8e7e25d8df95b7d0968db32447a39be49
Govind Vishwakarma <vishwakarmagovind22@gmail.com>
2020-12-25 21:25:21 +0530

    Added functionality using dummy data.

--- client/src/components/AddTransaction.jsx
+++ client/src/components/AddTransaction.jsx
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
+  const { addTransaction } = useContext(GlobalContext);
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount,
+    };
+    addTransaction(newTransaction);
+  };
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
