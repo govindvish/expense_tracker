85834e192bfd68ab3c85a16937a8c64d32f69294
Govind Vishwakarma <vishwakarmagovind22@gmail.com>
2020-12-26 17:34:01 +0530

    Integrated addTransaction api.

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -50,11 +50,26 @@ export const GlobalProvider = ({ children }) => {
     }
   };
 
-  const addTransaction = (transaction) => {
-    dispatch({
-      type: 'ADD_TRANSACTION',
-      payload: transaction,
-    });
+  const addTransaction = async (transaction) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.post(`/api/v1/transactions`, transaction, config);
+
+      dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: res.data.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: 'TRANSACTION_ERROR',
+        payload: err.response.data.error,
+      });
+    }
   };
 
   return (
